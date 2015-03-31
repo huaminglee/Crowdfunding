96c4f8fca6decbe4bada33a9a9efb8db8b7978bb
sunnycase <sunnycase@live.cn>
2015-03-31 13:52:26 +0800

    initial

---
+++ Crowdfunding/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Crowdfunding.Startup))]
+namespace Crowdfunding
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
